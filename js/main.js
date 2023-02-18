0c4a7a10b223dc9d2d892657246b3adbc257625f
Gulu Guluzade <mister.guluem25@gmail.com>
2023-02-18 07:26:13 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const loginBtn = document.querySelector('.login--btn');
+const hiddenLogin = document.querySelector('.hidden-login');
 
+loginBtn.addEventListener('click', () => {
+    hiddenLogin.classList.toggle('show-login');
+});
 
+const findOutChoice = document.querySelector('.findOut-choice');
+const hiddenFindOut = document.querySelector('.hidden-findOut');
 
+findOutChoice.addEventListener('click', () => {
+    hiddenFindOut.classList.toggle('show-findOut');
+});
